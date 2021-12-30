70976e42b78a011702c8e528cea8278ed59bf9d7
Anézio Rodrigues Junior <a.rodrigues.jr@live.com>
2021-12-30 09:42:17 -0300

    Exercicio prorposto para fixação de tratamento de exceções

---
+++ ExercicioProposto-Excecoes/Entities/Account.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using ExercicioProposto_Excecoes.Entities.Exceptions;
+
+
+namespace ExercicioProposto_Excecoes.Entities
+{
+    class Account
+    {
+        public string Holder { get; set; }
+        public int Number { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(string holder, int number, double balance, double withdrawLimit)
+        {
+            Holder = holder;
+            Number = number;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Withdraw(double withDraw)
+        {
+            if(withDraw > Balance)
+            {
+                throw new DomainException("Insufficient funds.");
+            }
+            else if(withDraw >= WithdrawLimit)
+            {
+                throw new DomainException("Withdraw limit reached.");
+            }
+            else
+                Balance -= withDraw;
+        }
+    }
+}
---
+++ ExercicioProposto-Excecoes/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Globalization;
+using ExercicioProposto_Excecoes.Entities;
+using ExercicioProposto_Excecoes.Entities.Exceptions;
+
+namespace ExercicioProposto_Excecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opt = 1;
+            switch (opt)
+            {
+                case 1:
+                    try
+                    {
+                        Console.Clear();
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine("Enter account data".ToUpper());
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine();
+                        Console.Write("Number: ");
+                        int inputNum = int.Parse(Console.ReadLine());
+
+                        Console.Write("Holder: ");
+                        string inputName = Console.ReadLine();
+
+                        Console.Write("Initial balance: ");
+                        double inputBalance = double.Parse(Console.ReadLine());
+
+                        Console.Write("Withdraw limit: ");
+                        double inputLimit = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine();
+                        Console.Write("Enter amount for withdraw: ".ToUpper());
+                        double inputWithdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                        Account acc = new Account(inputName, inputNum, inputBalance, inputLimit);
+                        acc.Withdraw(inputWithdraw);
+
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine();
+                        Console.WriteLine($"New balance for {acc.Holder}'s account:" +
+                            $" ${acc.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+                    }
+                    catch (DomainException e)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine("Withdraw error: " + e.Message);
+                        Console.WriteLine();
+                        Console.WriteLine("The program will restart in a few seconds.");
+                        Console.WriteLine("----------------------------------------");
+                        System.Threading.Thread.Sleep(3000);
+                        goto case 1;
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine("Format error: " + e.Message);
+                        Console.WriteLine();
+                        Console.WriteLine("The program will restart in a few seconds.");
+                        Console.WriteLine("----------------------------------------");
+                        System.Threading.Thread.Sleep(3000);
+                        goto case 1;
+                    }
+                    break;
+            }
+        }
+    }
+}
+
+
